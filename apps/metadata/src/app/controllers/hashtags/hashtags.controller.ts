91d8b8627e639ed2453e92188681ccb5c3eaee3e
Zong <zong09@hotmail.com>
2021-08-24 07:46:14 +0700

    update response message

--- apps/metadata/src/app/controllers/hashtags/hashtags.controller.ts
+++ apps/metadata/src/app/controllers/hashtags/hashtags.controller.ts
@@ -1,12 +1,8 @@
 import { HashtagService } from '@castcle-api/database';
 import { HashtagResponse } from '@castcle-api/database/dtos';
 import { CastLogger, CastLoggerOptions } from '@castcle-api/logger';
-import { CastcleException, CastcleStatus } from '@castcle-api/utils/exception';
-import {
-  CredentialInterceptor,
-  CredentialRequest
-} from '@castcle-api/utils/interceptors';
-import { Controller, Get, Req, UseInterceptors } from '@nestjs/common';
+import { CredentialInterceptor } from '@castcle-api/utils/interceptors';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { ApiBearerAuth, ApiHeader, ApiOkResponse } from '@nestjs/swagger';
 import { AppService } from '../../app.service';
 @Controller('hashtags')
@@ -21,33 +17,25 @@ export class HashtagsController {
     CastLoggerOptions
   );
 
-  @ApiBearerAuth()
   @ApiHeader({
     name: 'Accept-Language',
     description: 'Device prefered Language',
     example: 'th',
     required: true
   })
+  @ApiBearerAuth()
   @ApiOkResponse({
     type: HashtagResponse
   })
   @UseInterceptors(CredentialInterceptor)
   @Get()
-  async getData(@Req() req: CredentialRequest) {
+  async getAllHashTags() {
     this.logger.log('Start get all hashtag.');
-    if (
-      req.$credential.account.isGuest ||
-      !req.$credential.account.activateDate
-    )
-      throw new CastcleException(
-        CastcleStatus.FORBIDDEN_REQUEST,
-        req.$language
-      );
-
-    const content = await this.hashtagService.getAll();
+    const hashTag = await this.hashtagService.getAll();
     this.logger.log('Complete get all hashtag.');
     return {
-      payload: content.map((x) => x.toPagePayload())
+      message: 'success message',
+      payload: hashTag.map((x) => x.toPagePayload())
     } as HashtagResponse;
   }
 }
